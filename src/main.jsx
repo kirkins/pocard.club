1934964abb3b3a86cb20ae7b0e8d1650e50920e2
Philip Kirkbride <phk@2klic.com>
2018-01-14 17:21:27 -0500

    add amazon cognito login, react setup

---
+++ src/main.jsx
@@ -0,0 +1,75 @@
+import {Config, CognitoIdentityCredentials} from "aws-sdk";
+import {
+  CognitoUserPool,
+  CognitoUserAttribute
+} from "amazon-cognito-identity-js";
+import React from "react";
+import ReactDOM from "react-dom";
+import appConfig from "./config";
+
+Config.region = appConfig.region;
+Config.credentials = new CognitoIdentityCredentials({
+  IdentityPoolId: appConfig.IdentityPoolId
+});
+
+const userPool = new CognitoUserPool({
+  UserPoolId: appConfig.UserPoolId,
+  ClientId: appConfig.ClientId,
+});
+
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phone_number: '',
+      password: '',
+    };
+  }
+
+  handlePhoneChange(e) {
+    this.setState({phone_number: e.target.value});
+  }
+
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const phone_number = this.state.phone_number.trim();
+    const password = this.state.password.trim();
+    const attributeList = [
+      new CognitoUserAttribute({
+        Name: 'phone_number',
+        Value: phone_number,
+      })
+    ];
+    userPool.signUp(phone_number, password, attributeList, null, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('user name is ' + result.user.getUsername());
+      console.log('call result: ' + result);
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <input type="text"
+               value={this.state.phone_number}
+               placeholder="Phone"
+               onChange={this.handlePhoneChange.bind(this)}/>
+        <input type="password"
+               value={this.state.password}
+               placeholder="Password"
+               onChange={this.handlePasswordChange.bind(this)}/>
+        <input type="submit"/>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<SignUpForm />, document.getElementById('app'));
+
