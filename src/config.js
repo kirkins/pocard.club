72a077f2f1ceb70b3cf552751e47753ea03f1f68
Philip Kirkbride <phk@2klic.com>
2018-01-14 18:05:58 -0500

    make repo only compiled code, move source to private

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  region: 'us-east-1',
-  IdentityPoolId: 'us-east-1:ad5c3563-d818-41aa-9085-d3dbf6b56fee',
-  UserPoolId: 'us-east-1_M5ZgYCFtK',
-  ClientId: '54vpf1hkbe7emhlm0j8vmh17n2',
-}
